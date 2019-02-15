2aae3b5375bce38c7ed3ba6f418ecb295660ed52
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-15 17:01:32 +0200

    add macros,base templates,display content on root page

--- app/request.py
+++ app/request.py
@@ -3,7 +3,7 @@ import urllib.request,json
 from app.models import source,article
 
 Source=source.Source
-Artticle=article.Article
+Article=article.Article
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
@@ -25,3 +25,34 @@ def get_news(source):
      if get_news_response['articles']:
        news_results_list=get_news_response['articles']
        news_results=process_news(news_results_list)
+
+     return news_results
+
+def process_news(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of articles objects
+    '''
+
+    news_results=[]
+    for article in news_list:
+        author=article.get('author')
+        title=article.get('title')
+        description=article.get('description')
+        url=article.get('url')
+        imageUrl=article.get('urlToImage')
+
+        if imageUrl:
+          article_object=Article(author,title,description,url,imageUrl)
+          news_results.append(article_object)
+    return news_results    
+
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,8 +9,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    bitcoin_news=get_news('bitcoin')
+    tech_news=get_news('TechCrunch')
+    mash_news=get_news('Mashable')
+    cnn_news=get_news('CNN')
+
+
     title="Home- News"
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title, source1=bitcoin_news,source2=tech_news,source3=mash_news,source4=cnn_news)
 
 @app.route('/article/<article_url>')
 def article(article_url):
