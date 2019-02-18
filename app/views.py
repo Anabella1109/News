8d46162e999c0570b2aac5abf122407125348fae
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-18 11:08:13 +0200

    add new category in views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .models import source
+from .models import source,article
 from .request import get_news,get_source
 # Source=source.Source
 
@@ -11,28 +11,22 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    cat_general = get_source('general')
-    cat_business = get_source('business')
-    cat_entertainment = get_source('entertainment')
-    cat_sports = get_source('sports')
-    cat_tech = get_source('technology')
-    cat_science = get_source('science')
-    cat_health = get_source('health')
+    general_category = get_source('general')
+    technology_category = get_source('technology')
+    sciences_category = get_source('science')
+    entertainment_category = get_source('entertainment')
+    sports_category = get_source('sports')
+    health_category = get_source('health')
+    business_category = get_source('business')
 
-
-    # bitcoin_news=get_news('bitcoin')
-    # tech_news=get_news('TechCrunch')
-    # mash_news=get_news('Mashable')
-    # cnn_news=get_news('CNN')
 
 
     title="Home- News"
-    # return render_template('index.html', title=title, source1=bitcoin_news,source2=tech_news,source3=mash_news,source4=cnn_news)
-    return render_template('index.html',title=title, general=cat_general, business = cat_business, entertainment = cat_entertainment, sports = cat_sports, tech = cat_tech, science = cat_science, health = cat_health)
+    return render_template('index.html',title=title, general=general_category, business = business_category, entertainment = entertainment_category, sports = sports_category, technology = technology_category, science = sciences_category, health = health_category)
 
-@app.route('/article/<article_url>')
-def article(article_url):
-  '''
-  View article page function that article source page
-  '''
+# @app.route('/article/<article_url>')
+# def article(article_url):
+#   '''
+#   View article page function that article source page
+#   '''
    
