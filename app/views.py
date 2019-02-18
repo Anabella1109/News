e6b6d97ac404f54141a09c6920e221daee935d8c
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-18 15:34:39 +0200

    add link to css file in index.html

--- app/views.py
+++ app/views.py
@@ -24,14 +24,13 @@ def index():
     title="Home- News"
     return render_template('index.html',title=title, general=general_category, business = business_category, entertainment = entertainment_category, sports = sports_category, technology = technology_category, science = sciences_category, health = health_category)
 
-@app.route('/source/<source_id>')
-def sources(source_id):
+@app.route('/source/<source_name>')
+def sources(source_name):
   '''
   View article page function that article source page
   '''
 
-  articles=get_news(source_id)
-  print(source_id)
-  title=f'{source_id}'
-  return render_template('source.html',title=title,articles=articles)
+  articles=get_news(source_name)
+  title=f'{source.id}'
+  return render_template('source.html',title=title)
    
